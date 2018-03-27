048e7530ad4f37858c7af3a8354982d02ab12f6d
kryss <me@zhanshengjie.com>
2018-03-27 17:03:51 +0200

    add min and max to number input

--- src/inputs/input-number.component.ts
+++ src/inputs/input-number.component.ts
@@ -8,7 +8,7 @@ import { InlineNumberConfig } from "../types/inline-configs";
     template: `<input #inputRef type="number" class="form-control" (keyup.enter)="onEnter($event)"
                 (keyup.escape)="onEscape($event)" (focus)="onFocus($event)" (blur)="onBlur($event)" (blur)="onBlur($event)"
                 (keypress)="onKeyPress($event)" [(ngModel)]="value" [required]="config.required"
-                [disabled]="state.isDisabled()" [name]="config.name" [placeholder]="config.placeholder"
+                [disabled]="state.isDisabled()" [name]="config.name" [min]="config.min" [max]="config.max" [placeholder]="config.placeholder"
                 [size]="config.size"/>`,
                 changeDetection: ChangeDetectionStrategy.OnPush,
 })
